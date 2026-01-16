a0e1fa33144b8845c3105b927abe8019b6dc41ef
Anshul <anshul3071@gmail.com>
2026-01-16 16:19:10 +0530

    updated the IQ

--- playwright/tests/InterviewSpecific.spec.js
+++ playwright/tests/InterviewSpecific.spec.js
@@ -23,53 +23,20 @@ test.only('Practice pop-up',async ({page, context})=>{
     await popup.locator('#input').fill('Hey! there');
     //blank 4
     await popup.locator('#submit').click();
-})
+});
 
-// test('Interview specific question', async({page})=>{    
-//     // Download file
-//     const [download] = await Promise.all([
-//         page.waitForEvent('download'),
-//         page.locator('#downloadBtn').click()
-//     ])
-//     download.saveAs('downlaods/report.pdf')
+test('Get users API', async ({ request }) => {
+  const response = await request.get('https://www.google.com', {
+    headers: {
+      Authorization: 'Bearer token'
+    },
+    params : {
+        country : 'India'
+    }
+  });
 
-//     //New page
-//     const [page] = await Promise.all([
-//         context.waitForEvent('page'),
-//         page.click('#newUrl')
-//     ])
-//     await page.waitForLoadState()
+  expect(response.status()).toBe(200);
 
-//     //create new page from browser
-//     const context = await browser.newContext();
-//     // const page = await context.newPage();
-
-//     //upload a file
-//     page.locator('#fileUpload').setInputFiles('data/test.pdf')
-
-//     // How do you handle dialog boxes (alert/confirm/prompt)?
-//     page.on('dialog', async dialog =>{
-//         await dialog.accept(); 
-//         await dialog.reject();
-//     })
-
-//     // How do you handle iframes?
-//     const frame = page.frame({ name: 'myFrame' });
-//     await frame.fill('#email', 'test@mail.com');
-
-//     // How do you wait for navigation after a click?
-//     await Promise.all([
-//     page.waitForNavigation(),
-//     page.click('#login'),
-//     ]);
-
-//     //How to handle multiple pages
-//     const pages = context.pages();
-//     await pages[1].bringToFront();
-
-//     // How do you take a screenshot?
-//     await page.screenshot({ path: 'page.png' });
-
-//     // How do you wait for an element to appear?
-//     await page.waitForSelector('#result');
-// })
+  const body = await response.json();
+  expect(body.country).toBe('India');
+});
